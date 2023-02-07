db2f50572868685c1c23d4f8c0ee9db12776fa51
Eris <me@itseris.dev>
2023-02-07 01:37:43 -0600

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,13 @@
 export default defineNuxtConfig({
     css: [
         '@/assets/css/main.scss',
-    ]
+    ],
+    app: {
+        head: {
+            title: 'itseris.dev',
+            meta: [
+                { name: 'description', content: 'A fun little Nuxt project and personal site'}
+            ],
+        }
+    }
 })
